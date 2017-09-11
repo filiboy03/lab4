e796baf5e5955b6bed9d4cfde378eaa70ad9d191
filiboy03 <filiboy03@gmail.com>
2017-09-11 15:12:08 -0500

    commit lab6

---
+++ lesson6/App2/routes/newsletter.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+     // pass the csrfToken to the view 
+  res.render('newsletter',{title: 'Email form', csrftoken: req.csrfToken() })
+  
+});
+
+router.post('/', function(req, res, next) {
+    
+    req.assert('email','A valid email is required').notEmpty().isEmail();
+
+    var errors = req.validationErrors();
+    if(errors) return res.render('error',{message: 'Something Went Wrong', error:errors});
+    else  {
+         fs.appendFile('public/subscribers.txt'," "+ req.body.email, function(err){
+            if(err) throw err;
+           else res.render('thankyou',{email: req.body.email, title:'submission'});
+           
+         })
+       
+    }
+   
+  });
+
+module.exports = router;
